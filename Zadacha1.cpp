f3c241a02894c5b6bbdad77eb3efd8296ce213cc
Igor-33 <igorbazyk6@gmail.com>
2016-05-10 19:13:47 +0300

    Add files via upload

---
+++ Zadacha1.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+   int main()
+ {
+   float x, y;
+	  printf("\nVvedite x:");
+	  scanf("%f", &x);
+	  y = x * x;
+	  printf ("x=%.1f\ty=%.1f\n", x,y);
+ 
+    system("PAUSE");
+    return 0;
+  }
