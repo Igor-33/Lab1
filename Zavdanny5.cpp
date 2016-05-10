69a60c6fac2203eb51aa83a8747a30f1671ac5ad
Igor-33 <igorbazyk6@gmail.com>
2016-05-10 19:25:40 +0300

    Add files via upload

---
+++ Zavdanny5.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    float a, b;
+    printf("Vvediti a : ");
+    scanf("%f", &a);
+    printf ("%f", a);
+    printf("\nVvediti b : ");
+    scanf("%d", &b);
+    printf("%d", b);
+    
+    system("PAUSE");
+}
